122cd02512042eab2a99943a37f35d9726bf273d
rakesh.yadav@limetray.com <rakesh.yadav@limetray.com>
2016-02-04 18:43:19 -0500

    basic project scaffolding for Limetray IMS

---
+++ test/karma.conf.js
@@ -0,0 +1,28 @@
+console.log("No test commands right now");
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome', 'Firefox'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
